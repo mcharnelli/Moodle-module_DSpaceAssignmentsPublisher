9f9dde8805575e440a9ba8b628729a6ec03fdc79
Your Name <you@example.com>
2014-05-19 20:42:49 -0300

    mensajes en espanol

---
+++ lang/es/sword.php
@@ -0,0 +1,57 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * English strings for sword
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package    mod
+ * @subpackage sword
+ * @copyright  2011 Your Name
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'SWORD';
+$string['assignments_list']='Lista de Tareas';
+$string['assignment']='Tarea';
+$string['msg_error']='Hubo errores en el envío';
+$string['msg_send']='El envío se realizó con éxito';
+$string['modulenameplural'] = 'swords';
+$string['modulename_help'] = 'Use the sword module for... | The sword module allows...';
+$string['nosend']='No enviado'; 
+$string['send']='Enviado';
+$string['error']='Error al enviar'; 
+$string['repositoryurl'] = 'url del repositorio';
+$string['repository']='Información del repositorio ';
+$string['username']='usuario';
+$string['password']='contraseña';
+$string['metadata']='Valores de los metadatos por defecto';
+$string['subject']='Palabras claves';
+$string['rigths']='Derechos';
+$string['language']='Idioma';
+$string['publisher']='Publicador';
+$string['swordname'] = 'nombre';
+$string['msg-repository']="Complete la url con la ubicación de SWORD y el número del handle de la colección donde se depositará";
+$string['swordname_help'] = 'This is the content of the help tooltip associated with the swordname field. Markdown syntax is supported.';
+$string['sword'] = 'SWORD';
+$string['pluginadministration'] = 'sword administration';
+$string['pluginname'] = 'SWORD';
