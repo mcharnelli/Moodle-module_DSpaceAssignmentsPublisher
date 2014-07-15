4e86a9278b8a6f2ea3f0afe7491938d4b9de01a9
Maria Emilia Charnelli <maria.emilia.charnelli@gmail.com>
2014-07-15 23:39:25 +0000

    eliminaba carpeta test de la api sword

--- getCollections.php
+++ getCollections.php
@@ -5,4 +5,4 @@ $ch = curl_init($_POST["url"] . "/rest/collections/");
 curl_setopt($ch, CURLOPT_HEADER, 0); 
 $output = curl_exec($ch); 
 curl_close($ch); 
- json_encode($output);
+json_encode($output);
